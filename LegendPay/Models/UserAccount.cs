9ef139444bed52ac85a25952de73ed7828a1e1d0
codingnoobhelp <programmingwithkami@gmail.com>
2026-05-25 10:42:44 +0100

    Add project files.

---
+++ LegendPay/Models/UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LegendPay.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        //[DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Username cannot exceed 20 characters.")]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage = "Password is required.")]
+        //[DataType(DataType.Password)]
+        [MaxLength(20, ErrorMessage = "Password cannot exceed 20 characters.")]
+        public string Password { get; set; }
+    }
+}
