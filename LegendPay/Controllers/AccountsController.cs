023f381394f7c267065fe212fd883954f5cdd8fb
codingnoobhelp <programmingwithkami@gmail.com>
2026-05-27 08:45:47 +0100

    updated account controller

--- LegendPay/Controllers/AccountsController.cs
+++ LegendPay/Controllers/AccountsController.cs
@@ -66,7 +66,7 @@ namespace LegendPay.Controllers
                     await _emailService.SendOtpEmailAsync(account.Email, otp);
 
                     //store email in session for verification page
-                    TempData["VerifiactionEmail"] = account.Email; //to store the email in TempData so that it can be accessed in the verification page
+                    TempData["VerificationEmail"] = account.Email; //to store the email in TempData so that it can be accessed in the verification page
 
                     return RedirectToAction("VerifyEmail"); //redirect to the email verification page after successful sign-up
 
