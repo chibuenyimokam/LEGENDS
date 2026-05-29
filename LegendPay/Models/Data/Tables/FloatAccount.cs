6ee33bc13786908e221e9f063b6346dcf8eb8274
Adaku-Fav-Nwaeze <nwaeze.adaku@gmail.com>
2026-05-29 13:40:38 +0100

    updated models and migrations

---
+++ LegendPay/Models/Data/Tables/FloatAccount.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LegendPay.Models.Data.Tables
+{
+    // Only ONE float account for the entire platform
+    // Funded manually by admin, used to pay out cashback to users
+    public class FloatAccount
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Balance { get; set; } = 0;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<FloatTransaction>? Transactions { get; set; }
+    }
+}
---
+++ LegendPay/Models/Data/Tables/Receipt.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LegendPay.Models.Data.Tables
+{
+    public class Receipt
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid BillId { get; set; }
+
+        [ForeignKey(nameof(BillId))]
+        public Bill Bill { get; set; }
+
+        [Required]
+        public Guid UserAccountId { get; set; }
+
+        [ForeignKey(nameof(UserAccountId))]
+        public UserAccount UserAccount { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ReceiptNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string BillerName { get; set; }
+
+        [MaxLength(200)]
+        public string? ConfirmationToken { get; set; }
+
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+    }
+}
