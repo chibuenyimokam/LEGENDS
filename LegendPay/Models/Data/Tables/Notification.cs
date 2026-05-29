84c2045fa8f110ed138949cb4ff7ad2cd79a78bd
Adaku-Fav-Nwaeze <nwaeze.adaku@gmail.com>
2026-05-29 13:40:38 +0100

    updated models and migrations

---
+++ LegendPay/Models/Data/Tables/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LegendPay.Models.Data.Tables
+{
+    public class Notification
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid UserAccountId { get; set; }
+
+        [ForeignKey(nameof(UserAccountId))]
+        public UserAccount UserAccount { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; } // "WalletFunded", "BillPaid", "Reminder", "AutoPay", "CashbackCredited", "LowBalance"
+
+        [Required]
+        [MaxLength(300)]
+        public string Message { get; set; }
+
+        public bool IsRead { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
