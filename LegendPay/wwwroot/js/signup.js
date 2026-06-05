8ade605a1ee9ef5f3614f3220a80b375eac6b2a9
chibuenyimokam <chibuenyimokam@gmail.com>
2026-06-05 11:20:11 +0100

    Add styled login page with password toggle, edit signup page checkbox and padding fixes, responsive design

---
+++ LegendPay/wwwroot/js/signup.js
@@ -0,0 +1,12 @@
+﻿function toggleWallet() {
+    const checkmark = document.getElementById('checkmark');
+    const checkbox = document.getElementById('walletCheckbox');
+
+    if (checkmark.style.display === 'none') {
+        checkmark.style.display = 'block';
+        checkbox.style.background = 'var(--blue)';
+    } else {
+        checkmark.style.display = 'none';
+        checkbox.style.background = 'transparent';
+    }
+}
