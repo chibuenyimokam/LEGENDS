9bb1e15546780b659dc3060358d486f200c545c9
codingnoobhelp <programmingwithkami@gmail.com>
2026-07-20 23:35:12 +0100

    fixes

--- LegendPay/Interfaces/Auth/IAuthService.cs
+++ LegendPay/Interfaces/Auth/IAuthService.cs
@@ -15,6 +15,8 @@ namespace LegendPay.Interfaces.Auth
         Task<UserAccount?> GetUserByEmailAsync(string email);
         Task<UserAccount?> GetUserByIdAsync(Guid userId);
         Task<UserAccount?>CreateAndSaveUserAsync(SignUpViewModel model, string initialotp);
+        Task<bool> GeneratePasswordResetAsync(string email, string otp);
+        Task<bool> ResetPasswordAsync(string email, string otp, string newPassword);
         Task<UserAccount?> ValidateLoginCredentialsAsync(string identifier, string plainPassword);
         Task UpdateUserAsync(UserAccount user);
         Task<decimal?> GetUserBalanceAsync(string email);
